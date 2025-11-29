333acdf3c0703a5d0adbc108a43956051348b937
Mads Adrian Simonsen <47949834+simadma@users.noreply.github.com>
2025-11-29 17:35:30 +0000

    disable nvim, tmux and rm jupyter

--- install.sh
+++ install.sh
@@ -11,23 +11,9 @@ cd "$(dirname "$0")"
 source scripts/git_sign_config.sh
 
 # Install tmux
-source scripts/install_tmux.sh
+# source scripts/install_tmux.sh
 
 # Install Neovim
-source scripts/install_neovim.sh
+# source scripts/install_neovim.sh
 
-# Add Jupyter Lab config
-mkdir -p ~/.config/jupyter
-cp -r config/jupyter/* ~/.config/jupyter/
-
-# Set Jupyter config path
-echo 'export JUPYTER_CONFIG_DIR=~/.config/jupyter' >> ~/.bash_profile
-
-# Set Neovim as default text editor
-echo 'export EDITOR=nvim' >> ~/.bash_profile
-echo 'export VISUAL=nvim' >> ~/.bash_profile
-
-# Make sure to run ~/.bashrc when connecting via SSH
-echo 'source ~/.bashrc > /dev/null' >> ~/.bash_profile
-
-echo -e "\nInstallation complete!"
+# echo -e "\nInstallation complete!"
